e5702c1742e50d4a43a2244511c08ffe17937bbc
Kimchifriedricegg <alexovidenie@gmail.com>
2016-08-15 16:17:11 -0400

    added

--- exercise2.rb
+++ exercise2.rb
@@ -1,7 +1,8 @@
 class Item
-end
+
+@@items = []
 @@tax = 0
-  def initialize(cat, q, type, price)
+  def initialize(q, type, price, cat)
     @q = q
     @type = type
     @price = price
@@ -10,15 +11,22 @@ end
 
   def sales_tax
     @@tax = 0
-    if @cat (== "books" || "medicine" || "food") && @type
-      @@tax = @price * 0.05
-    elsif @cat (!== "books" || "medicine" || "food" && !@type
-      @@tax = @@price * 0.10
-    elsif @cat (!== "books" || "medicine" || "food" && @type
-      @@tax = @price* 0.15
+    if @cat == ("books" || @cat == "medicine" || @cat == "food") && @type
+       @@tax = @price * 0.05
+    elsif @cat != "books" || @cat == "medicine" || @cat == "food" && !@type
+       @@tax = @@price * 0.10
+    elsif @cat != "books" || @cat == "medicine" || @cat == "food" && @type
+       @@tax = @price * 0.15
     else
       @@tax = 0
     end
+  end
 
-class Receipt
+  def self.all
+    @@items
+
+    puts "#{@@items}"
 end
+end
+
+book = Item.new(1, "false", 1.49, "book")
